064ac830aa7503606a835ecf2a89de30569ae628
Imabanana80 <imabanana80.hallo@gmail.com>
2023-08-02 10:14:32 +0800

    update fancy redirect

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -1,6 +1,8 @@
-const queryString = window.location.search;
+document.addEventListener("DOMContentLoaded", () => {
+  const queryString = window.location.search;
 
-const urlParams = new URLSearchParams(queryString);
+  const urlParams = new URLSearchParams(queryString);
 
-const target = urlParams.get("to");
-location.replace(target);
+  const target = urlParams.get("to");
+  location.replace(target);
+});
