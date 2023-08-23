56a4497d610afabe90bbaffa8766a8a3a7b3d2f0
Imabanana80 <imabanana80.hallo@gmail.com>
2023-08-23 18:22:51 +0800

    added valid urls for redirect endpoint (so that people dont abuse lol)

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -4,5 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(queryString);
 
   const target = urlParams.get("to");
-  location.replace(target);
+
+  const valid = [
+    "https://twitch.tv/Imabanana80",
+    "https://github.com/Imabanana80",
+    "https://youtube.com/@Imabanana80",
+    "https://discord.gg/Hz5TwPs8Rv",
+    "https://modrinth.com/user/wZCAi9ke",
+    "https://thegerald.net/",
+  ];
+
+  if (valid.includes(target)) {
+    location.replace(target);
+  } else {
+    location.replace("https://imabanana80.com/404");
+  }
 });
