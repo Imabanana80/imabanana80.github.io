11ef5884d0f702d27f362617e78a7456ad7c5bca
Imabanana80 <imabanana80.hallo@gmail.com>
2024-02-23 21:18:50 +0800

    switchy switchy

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "https://twitch.tv/Imabanana80",
     "https://github.com/Imabanana80",
     "https://youtube.com/@Imabanana80",
-    "https://discord.gg/Hz5TwPs8Rv",
+    "https://discord.gg/FVvRMAx2Sm",
     "https://modrinth.com/user/wZCAi9ke",
     "https://thegerald.net",
     "https://github.com/Imabanana80/imabanana80.github.io",
